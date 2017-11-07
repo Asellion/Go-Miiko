21d13d6eff265b5155d09952ed0e71ee42c847a3
Nato Boram <nato.boram@gmail.com>
2017-11-07 03:09:10 -0500

    it works

--- bot/bot.go
+++ bot/bot.go
@@ -2,6 +2,7 @@ package bot
 
 import (
 	"fmt"
+	"strings"
 
 	"../config"
 	"github.com/bwmarrin/discordgo"
@@ -49,7 +50,103 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-	if m.Content == "ping" {
-		_, _ = s.ChannelMessageSend(m.ChannelID, "pong")
+	// Get channel structure
+	channel, err := s.State.Channel(m.ChannelID)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	// Get guild structure
+	guild, err := s.State.Guild(channel.GuildID)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	// Get guild member
+	member, err := s.GuildMember(channel.GuildID, m.Author.ID)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	// If Author has no role
+	if len(member.Roles) != 0 {
+		return
+	}
+
+	// Get mentionned roles
+	var gardes []string
+	if strings.Contains(strings.ToLower(m.Content), "tincelant") {
+		gardes = append(gardes, "Étincelante")
+	}
+	if strings.Contains(strings.ToLower(m.Content), "obsi") {
+		gardes = append(gardes, "Obsidienne")
+	}
+	if strings.Contains(strings.ToLower(m.Content), "absy") {
+		gardes = append(gardes, "Absynthe")
+	}
+	if strings.Contains(strings.ToLower(m.Content), "ombr") {
+		gardes = append(gardes, "Ombre")
+	}
+	if strings.Contains(strings.ToLower(m.Content), "joue pas") || strings.Contains(strings.ToLower(m.Content), "aucun") {
+		gardes = append(gardes, "PNJ")
+	}
+
+	// Check if there's only one mentionned role
+	var garde string
+	if len(gardes) == 1 {
+		garde = gardes[0]
+	}
+
+	// Announce
+	if garde == "Étincelante" {
+		_, err := s.ChannelMessageSend(m.ChannelID, "Si tu fais partie de la Garde Étincelante, envoie un message à <@"+guild.OwnerID+"> sur Eldarya pour annoncer ta présence.")
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+	if garde == "Obsidienne" || garde == "Absynthe" || garde == "Ombre" {
+		roleID := getRoleByName(s, channel.GuildID, garde)
+		s.GuildMemberRoleAdd(channel.GuildID, m.Author.ID, roleID)
+		_, err := s.ChannelMessageSend(m.ChannelID, "Bienvenue dans la Garde <@&"+roleID+">!")
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+}
+
+func getRoleByName(s *discordgo.Session, guildID string, name string) string {
+
+	// Get roles
+	guildRoles, err := s.GuildRoles(guildID)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	// Get the first occurence
+	for x := 0; x < len(guildRoles); x++ {
+		if guildRoles[x].Name == name {
+			return guildRoles[x].ID
+		}
+	}
+
+	return ""
+}
+
+func joinHandler(s *discordgo.Session, j *discordgo.GuildMemberAdd) {
+
+	// Get the guild structure
+	guild, err := s.State.Guild(j.GuildID)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	// Get ChannelMessageSend parameters
+	channelID := guild.EmbedChannelID
+	message := "Bonjour " + j.User.Mention() + "! T'es dans quelle garde?"
+
+	// Ask newcomer what's their guard
+	_, err = s.ChannelMessageSend(channelID, message)
+	if err != nil {
+		fmt.Println(err.Error())
 	}
 }
