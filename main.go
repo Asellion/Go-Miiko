f7361942dff0b8915fc82bc916931a3830f7316a
Nato Boram <nato.boram@gmail.com>
2017-11-06 20:29:28 -0500

    config

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"./bot"
+	"./config"
+	"fmt"
+)
+
+func main() {
+	err := config.ReadConfig()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	bot.Start()
+
+	<-make(chan struct{})
+	return
+}
