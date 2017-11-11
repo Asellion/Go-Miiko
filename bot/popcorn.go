892416ba9e13c50646539d38eb12e134d117d172
Nato Boram <nato.boram@gmail.com>
2017-11-11 17:35:55 -0500

    NANI ?!?

--- bot/popcorn.go
+++ bot/popcorn.go
@@ -39,6 +39,17 @@ func popcorn(s *discordgo.Session, m *discordgo.MessageCreate) {
 			}
 		}
 	}
+
+	// Check for "Omae Wa Mou Shindeiru"
+	if strings.Contains(strings.ToLower(m.Content), "omae wa mou shindeiru") {
+
+		// Nani?!
+		_, err := s.ChannelMessageSend(m.ChannelID, getNaniMessage())
+		if err != nil {
+			fmt.Println("Couldn't express my surprize. Sad :(")
+			fmt.Println(err.Error())
+		}
+	}
 }
 
 func getPopcornMessage() string {
@@ -75,4 +86,26 @@ func getPopcornMessage() string {
 
 	// Return
 	return popcornList[rand.Intn(len(popcornList))]
+}
+
+func getNaniMessage() string {
+
+	// Popcorn Messages
+	var naniList []string
+
+	naniList = append(naniList, "Nani?")
+	naniList = append(naniList, "Nani?!")
+	naniList = append(naniList, "Nani!?")
+	naniList = append(naniList, "**Nani?!**")
+	naniList = append(naniList, "**Nani!?**")
+	naniList = append(naniList, "**Nani ?!?**")
+	naniList = append(naniList, "**Nani !?!**")
+
+	// Seed
+	seed := time.Now().UnixNano()
+	source := rand.NewSource(seed)
+	rand := rand.New(source)
+
+	// Return
+	return naniList[rand.Intn(len(naniList))]
 }
