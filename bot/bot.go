7d85d388a487c2ead9314ca493019de1ba6714c7
Nato Boram <nato.boram@gmail.com>
2017-12-12 16:16:34 -0500

    removed useless debug

--- bot/bot.go
+++ bot/bot.go
@@ -15,16 +15,12 @@ var goBot *discordgo.Session
 // Start : Starts the bot.
 func Start() {
 
-	fmt.Println("Begin START")
-
 	// Go online!
 	goBot, err := discordgo.New("Bot " + config.Token)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
-
-	fmt.Println("Token : " + config.Token)
 
 	// Get Bot ID
 	u, err := goBot.User("@me")
@@ -34,8 +30,6 @@ func Start() {
 		return
 	}
 	BotID = u.ID
-
-	fmt.Println("Bot ID : " + BotID)
 
 	// Hey, listen!
 	goBot.AddHandler(messageHandler)
@@ -55,10 +49,6 @@ func Start() {
 }
 
 func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
-
-	// Debug
-	fmt.Println("Author : " + m.Author.Username)
-	fmt.Println("Author : " + m.Content)
 
 	// Myself?
 	if m.Author.ID == BotID || m.Author.Bot {
