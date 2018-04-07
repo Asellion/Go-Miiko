26a0dc795947a34f7a956f071caf356662cbb110
Nato Boram <nato.boram@gmail.com>
2018-04-07 16:02:53 -0400

    Check for DB and Discord connection first

--- main.go
+++ main.go
@@ -1,32 +1,75 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
 
 	"github.com/NatoBoram/Go-Miiko/bot"
 	"github.com/NatoBoram/Go-Miiko/config"
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	database config.DBStruct
+	discord  config.DiscordStruct
+	db       *sql.DB
+	session  *discordgo.Session
 )
 
 func main() {
 
-	// Reads the JSON database
-	err := config.ReadJSON()
+	// Read the database config
+	err := config.ReadDB(&database)
 	if err != nil {
-		fmt.Println(err.Error())
-		config.WriteNewJSON()
+		println("Could not load the database configuration.")
+		println(err.Error())
+		config.WriteTemplateDB()
+		return
+	}
+
+	// Read the Discord config
+	err = config.ReadDiscord(&discord)
+	if err != nil {
+		println("Could not load the Discord configuration.")
+		println(err.Error())
+		config.WriteTemplateDiscord()
+		return
+	}
+
+	// Connect to MariaDB
+	db, err = sql.Open("mysql", database.User+":"+database.Password+"@tcp("+database.Address+":"+database.Port+")/"+database.Database)
+	if err != nil {
+		println("Could not connect to the database.")
+		println(err.Error())
+		return
+	}
+	defer db.Close()
+
+	// Create a Discord session
+	session, err = discordgo.New("Bot " + discord.Token)
+	if err != nil {
+		println("Could not create a Discord session.")
+		println(err.Error())
+		return
+	}
+
+	// Connect to Discord
+	err = session.Open()
+	if err != nil {
+		println("Could not connect to Discord.")
+		println(err.Error())
 		return
 	}
 
 	// License
-	fmt.Println("")
-	fmt.Println("Go-Miiko : Manages an Eldarya-themed Discord server.")
-	fmt.Println("Copyright © 2017 Nato Boram")
-	fmt.Println("This program is free software : you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY ; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.")
-	fmt.Println("Contact : https://github.com/NatoBoram/Go-Miiko")
-	fmt.Println("")
+	println("")
+	println("Go-Miiko : Manages an Eldarya-themed Discord server.")
+	println("Copyright © 2018 Nato Boram")
+	println("This program is free software : you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY ; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.")
+	println("Contact : https://github.com/NatoBoram/Go-Miiko")
+	println("")
 
 	// Give this bot some life!
-	bot.Start()
+	bot.Start(db, session)
 
 	// Wait for future input
 	<-make(chan struct{})
