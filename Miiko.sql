3970e01c263368df402aa2bcbda424af812b915c
Nato Boram <nato.boram@gmail.com>
2018-04-08 20:38:26 -0400

    Love view now returns the correct values

--- Miiko.sql
+++ Miiko.sql
@@ -1,20 +1,20 @@
-drop database `miiko`;
-create database if not exists `miiko` default character set utf8;
-use `miiko`;
+drop database `Miiko`;
+create database if not exists `Miiko` default character set utf8;
+use `Miiko`;
 
 -- Drop View
 -- drop view `love`;
 
 -- Drop Table
 drop table if exists `pins`;
-drop table if exists `servers`;
+drop table if exists `welcome`;
 
 -- Create Table
 
 -- Servers
-create table if not exists `servers` (
+create table if not exists `welcome` (
 	`server` varchar(32) primary key,
-	`welcome` varchar(32) not null
+	`channel` varchar(32) not null
 ) engine=InnoDB default charset=utf8;
 
 -- Pins
@@ -26,25 +26,33 @@ create table if not exists `pins` (
 
 -- Views
 
+-- Pins Count
+create view `pins-count` as
+select `server`, `member`, count(`message`) as `count`
+from `pins`
+group by `server`, `member`
+;
+
 -- Love
 create view `love` as
-select `server`, `member`
-from (
-	select `server`, `member`, count(`message`) as `count`
-	from `pins`
-	group by `server`, `member`
-) as `pins-count`
+select `server`, `member`, `count`
+from `pins-count` as `p1`
+where `p1`.`count` = (
+	select max(`count`) as `max`
+	from `pins-count` as `p2`
+	where `p1`.`server` = `p2`.`server`
+)
 group by `server`
 ;
 
 -- Test Values
 
 -- Servers
-INSERT INTO `servers`(`server`) VALUES("1");
-INSERT INTO `servers`(`server`) VALUES("2");
-INSERT INTO `servers`(`server`) VALUES("3");
-INSERT INTO `servers`(`server`) VALUES("4");
-INSERT INTO `servers`(`server`) VALUES("5");
+INSERT INTO `welcome`(`server`, `channel`) VALUES("1", "1");
+INSERT INTO `welcome`(`server`, `channel`) VALUES("2", "2");
+INSERT INTO `welcome`(`server`, `channel`) VALUES("3", "3");
+INSERT INTO `welcome`(`server`, `channel`) VALUES("4", "4");
+INSERT INTO `welcome`(`server`, `channel`) VALUES("5", "5");
 
 -- Pins
 INSERT into `pins` VALUES ("1", "1", "1");
