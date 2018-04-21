6cff2194cdfb222f12970caf3350ab5381671949
Nato Boram <nato.boram@gmail.com>
2018-04-21 16:11:58 -0400

    Better refresh

--- bot/startup.go
+++ bot/startup.go
@@ -13,11 +13,50 @@ func refresh(db *sql.DB, s *discordgo.Session) {
 	// Wait for a minute
 	time.Sleep(time.Minute)
 
-	// Begin
+	// Start
 	start := time.Now()
 	fmt.Println("Begin refresh :", start.String())
 
+	// Begin
+	tx, err := db.Begin()
+	if err != nil {
+		fmt.Println("Couldn't begin a pin transaction.")
+		fmt.Println(err.Error())
+		return
+	}
+
+	// Delete
+	result, err := tx.Exec("delete from `pins`;")
+	if err != nil {
+		fmt.Println("Couldn't delete all pins.")
+		fmt.Println(err.Error())
+		return
+	}
+
+	// Rows Affected
+	rows, err := result.RowsAffected()
+	if err != nil {
+		fmt.Println("Couldn't get all affected pins.")
+		fmt.Println(err.Error())
+		return
+	}
+
+	// Log
+	fmt.Println("Deleted", rows, "pins.")
+
+	// Prepare
+	insert, err := tx.Prepare("insert into `pins`(`server`, `member`, `message`) values(?, ?, ?)")
+	if err != nil {
+		fmt.Println("Couldn't prepare pins.")
+		fmt.Println(err.Error())
+		return
+	}
+	defer insert.Close()
+
+	// For each guilds
 	for _, guild := range s.State.Guilds {
+
+		// For each channels
 		for _, channel := range guild.Channels {
 
 			// Ignore non-text channels
@@ -35,11 +74,24 @@ func refresh(db *sql.DB, s *discordgo.Session) {
 				continue
 			}
 
-			// Pin
+			// For each pin
 			for _, message := range pins {
-				pindb(guild, message)
+
+				// Insert it!
+				_, err := insert.Exec(guild.ID, message.Author.ID, message.ID)
+				if err != nil {
+					fmt.Println("Couldn't execute a pin.")
+					fmt.Println(err.Error())
+				}
 			}
 		}
+	}
+
+	// Commit
+	err = tx.Commit()
+	if err != nil {
+		fmt.Println("Couldn't commit a pin transaction.")
+		fmt.Println(err.Error())
 	}
 
 	// End
