78b842680c1c37bfe8bf4421e09a07122e768796
Nato Boram <nato.boram@gmail.com>
2018-04-22 09:11:24 -0400

    Don't mention her lover

--- bot/love.go
+++ bot/love.go
@@ -32,9 +32,23 @@ func love(s *discordgo.Session, g *discordgo.Guild, c *discordgo.Channel, m *dis
 		// Rate Limit
 		if random < 1/(wheel.Phi()*10) {
 
+			// Member
+			member, err := s.GuildMember(g.ID, lover.ID)
+			if err != nil {
+				fmt.Println("Couldn't get the member I love.")
+				fmt.Println("Guild :", g.Name)
+				fmt.Println("Channel :", c.Name)
+				fmt.Println("User :", m.Author.Username)
+				fmt.Println("Message :", m.Content)
+				fmt.Println(err.Error())
+				return false
+			}
+
+			mention := "**" + member.Nick + "**"
+
 			// Give some love!
 			s.ChannelTyping(c.ID)
-			_, err = s.ChannelMessageSend(c.ID, getLoveMessage(m.Author))
+			_, err = s.ChannelMessageSend(c.ID, getLoveMessage(mention))
 			if err != nil {
 				fmt.Println("Couldn't express my love.")
 				fmt.Println("Guild :", g.Name)
@@ -50,57 +64,57 @@ func love(s *discordgo.Session, g *discordgo.Guild, c *discordgo.Channel, m *dis
 	return false
 }
 
-func getLoveMessage(u *discordgo.User) string {
+func getLoveMessage(name string) string {
 
 	// Messages
 	loveList := [...]string{
 
 		// Greetings
-		"Coucou <@" + u.ID + "> :3",
-		"Coucou <@" + u.ID + ">! \\*-*",
-		"Salut les gens... Oh! <@" + u.ID + ">! :heart:",
-		"Bonjour... Oh! <@" + u.ID + ">! :heart:",
-		"Coucou tout le monde... Oh! <@" + u.ID + ">! :heart:",
+		"Coucou " + name + " :3",
+		"Coucou " + name + "! \\*-*",
+		"Salut les gens... Oh! " + name + "! :heart:",
+		"Bonjour... Oh! " + name + "! :heart:",
+		"Coucou tout le monde... Oh! " + name + "! :heart:",
 		"Coucou mon amour!",
 
 		// Orders
-		"Tiens-moi la main, <@" + u.ID + ">",
-		"<@" + u.ID + ">! Regarde-moiii \\*-*",
-		"<@" + u.ID + "> : La perfection absolue.",
+		"Tiens-moi la main, " + name + "",
+		"" + name + "! Regarde-moiii \\*-*",
+		"" + name + " : La perfection absolue.",
 		"Caresse-moi les oreilles, s'il te plait!",
 
 		// Questions
-		"<@" + u.ID + ">... Tu veux qu'on fasse quelque chose ensemble?",
-		"Oh, <@" + u.ID + ">, est-ce que je te manque?",
-		"Est-ce que tu penses à moi, <@" + u.ID + ">?",
-		"<@" + u.ID + ">, me demanderas-tu ma main un jour..?",
-		"<@" + u.ID + ">, j'ai fait du popcorn, tu veux en manger avec moi? :3",
-		"<@" + u.ID + ">! Je suis là! Je t'ai manqué, n'est-ce pas? :smile:",
-		"<@" + u.ID + ">! Es-tu content du matelas que j'ai fait mettre dans ta chambre? J'ai dormi dessus :blush:",
+		"" + name + "... Tu veux qu'on fasse quelque chose ensemble?",
+		"Oh, " + name + ", est-ce que je te manque?",
+		"Est-ce que tu penses à moi, " + name + "?",
+		"" + name + ", me demanderas-tu ma main un jour..?",
+		"" + name + ", j'ai fait du popcorn, tu veux en manger avec moi? :3",
+		"" + name + "! Je suis là! Je t'ai manqué, n'est-ce pas? :smile:",
+		"" + name + "! Es-tu content du matelas que j'ai fait mettre dans ta chambre? J'ai dormi dessus :blush:",
 
 		// Reactions
 		":heart:",
 		"\\*Frissonne*",
 		"\\*-*",
-		"<@" + u.ID + ">-senpai \\*-*",
+		"" + name + "-senpai \\*-*",
 
 		// Verbose
-		"J'ai trouvé un morceau de cristal pour toi, <@" + u.ID + "> :heart:",
+		"J'ai trouvé un morceau de cristal pour toi, " + name + " :heart:",
 		"Cette voix est une musique à mes oreilles",
-		"J'aimerais pouvoir passer plus de temps avec toi, <@" + u.ID + ">...",
-		"Je fais juste passer pour dire à <@" + u.ID + "> que je l'aime!",
+		"J'aimerais pouvoir passer plus de temps avec toi, " + name + "...",
+		"Je fais juste passer pour dire à " + name + " que je l'aime!",
 		"J'adore quand tu parles... :3",
 		"J'adore entendre mon amour parler \\*-*",
-		"Mais quelle est cette douce musique? ... Oh! C'est la voix de <@" + u.ID + ">!",
+		"Mais quelle est cette douce musique? ... Oh! C'est la voix de " + name + "!",
 
 		// Actions
-		"\\*Pense à <@" + u.ID + ">*",
-		"\\*Regarde <@" + u.ID + ">*",
-		"\\*Se languis de <@" + u.ID + ">*",
+		"\\*Pense à " + name + "*",
+		"\\*Regarde " + name + "*",
+		"\\*Se languis de " + name + "*",
 
 		// Also fits in Command
-		"<@" + u.ID + ">, je t'aime!",
-		"Aaah... <@" + u.ID + ">!",
+		"" + name + ", je t'aime!",
+		"Aaah... " + name + "!",
 	}
 
 	// Seed
